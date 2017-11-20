5eedd860ce2160d416822a35663d956f7de1bff9
y0m0 <lixi.daniele@gmail.com>
2017-11-20 19:21:56 +0000

    Pass a tagline as a props to the header set the header and display the anchor icon

--- src/components/app.js
+++ src/components/app.js
@@ -3,13 +3,12 @@ import Header from './header'
 import Order from './order'
 import Inventory from './inventory'
 
-
 class App extends React.Component {
   render() {
     return (
     <div className="catch-of-the-day">
       <div className="menu">
-        <Header />
+        <Header tagline="Fresh Seafood Market" />
       </div>
       <Order />
       <Inventory />
