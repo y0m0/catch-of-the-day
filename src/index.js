5c2b956cd7f5e48b161f6e1eac0fe3c1bf32077b
y0m0 <lixi.daniele@gmail.com>
2017-11-20 18:17:35 +0000

    start project create simple component app

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { render } from 'react-dom'
+import './css/style/.css'
+
+
+import StorePicker from './components/storepicker'
+
+
+render(<StorePicker/>, document.querySelector('#main'))
