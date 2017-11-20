2262ac839553a0eb128fd9f97cca041f12111b76
y0m0 <lixi.daniele@gmail.com>
2017-11-20 18:42:35 +0000

    Add Header, Order, and Inventory components

--- src/components/app.js
+++ src/components/app.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import Header from './header'
+import Order from './order'
+import Inventory from './inventory'
 
-class app extends React.Component {
+
+class App extends React.Component {
   render() {
     return (
+    <div className="catch-of-the-day">
+      <div className="menu">
+        <Header />
+      </div>
+      <Order />
+      <Inventory />
+    </div>
     )
   }
 }
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { render } from 'react-dom'
-import './css/style/.css'
-
+import './css/style.css'
+import App from './components/app'
 
 import StorePicker from './components/storepicker'
 
 
-render(<StorePicker/>, document.querySelector('#main'))
+render(<App />, document.querySelector('#main'))
