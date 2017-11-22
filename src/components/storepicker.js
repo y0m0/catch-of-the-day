74c474e9c63543f57bfb20347479fd9a0d79085b
y0m0 <lixi.daniele@gmail.com>
2017-11-22 00:28:40 +0000

    Create routes for "/" and "store/:storeId" Implement a NotFound component

--- src/components/storepicker.js
+++ src/components/storepicker.js
@@ -3,7 +3,7 @@ import React from 'react'
 class StorePicker extends React.Component {
   render() {
     return (
-      <form classname="store-selector">
+      <form className="store-selector">
         <h2>Please Enter A Store</h2>
         <input type="text" required placeholder="Store Name"/>
         <button type="submit">Visit Store -></button>
--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
-import React from 'react'
-import { render } from 'react-dom'
-import './css/style.css'
-import App from './components/app'
-
-import StorePicker from './components/storepicker'
+import React from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter, Match, Miss } from 'react-router';
 
 
-render(<App />, document.querySelector('#main'))
+import './css/style.css';
+import App from './components/app';
+
+import StorePicker from './components/storepicker';
+import NotFound from './components/notfound'
+
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Match exactly pattern="/" component={StorePicker} />
+        <Match pattern="/store/:storeId" component={App} />
+        <Miss component={NotFound} />
+      </div>
+    </BrowserRouter>
+  )
+}
+
+
+render(<Root />, document.querySelector('#main'));
