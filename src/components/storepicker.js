9bb368c5f65a454a647f6c26dd9998dc9d13c801
y0m0 <lixi.daniele@gmail.com>
2017-11-23 19:04:01 +0000

    Add routes handling and new not found component

--- src/components/storepicker.js
+++ src/components/storepicker.js
@@ -1,16 +1,31 @@
-import React from 'react'
-import { getFunName } from '../helpers'
+import React from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+  // constructor() {
+  //   super();
+  //   this.goToStore = this.goToStore.bind(this);
+  // }
+
+  goToStore(event) {
+    event.preventDefault();
+    const storeId = this.storeInput.value;
+    this.context.router.transitionTo(`/store/${storeId}`);
+  }
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={(event) => this.goToStore(event)}>
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
+        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => {this.storeInput = input}} />
         <button type="submit">Visit Store -></button>
       </form>
     )
   }
+}
+
+StorePicker.contextTypes = {
+  router: React.PropTypes.object
 }
 
 export default StorePicker;
