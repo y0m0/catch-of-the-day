2564fcaa8bc1cb03f7ebf407aa2760a759b16b94
y0m0 <daniele@danieles-MacBook-Pro.local>
2017-11-25 19:43:14 +0000

    Can now add fishes to the order create a addToOrder function

--- src/components/app.js
+++ src/components/app.js
@@ -3,6 +3,7 @@ import Header from './header';
 import Order from './order';
 import Inventory from './inventory';
 import sampleFishes from '../sample-fishes';
+import Fish from './fish'
 
 
 class App extends React.Component {
@@ -11,6 +12,7 @@ class App extends React.Component {
 
     this.addFish = this.addFish.bind(this);
     this.loadSamples = this.loadSamples.bind(this);
+    this.addToOrder = this.addToOrder.bind(this);
 
     this.state =  {
       fishes: {},
@@ -34,11 +36,27 @@ class App extends React.Component {
     });
   }
 
+  addToOrder(key) {
+    // make a copy of the current state
+    const order = {...this.state.order};
+    // update or add the number of fish ordered
+    order[key] = order[key] + 1 || 1;
+    // update state
+    this.setState({ order });
+  }
+
   render() {
     return (
     <div className="catch-of-the-day">
       <div className="menu">
         <Header tagline="Fresh Seafood Market" />
+        <ul className="list-of-fishes">
+          {
+            Object
+              .keys(this.state.fishes)
+              .map(key => <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder} />)
+          }
+        </ul>
       </div>
       <Order />
       <Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
