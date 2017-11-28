9cc8a16112ae159229c94c3dcc5e3fa48819965f
y0m0 <lixi.daniele@gmail.com>
2017-11-28 11:32:43 +0000

    Add edit functionality to the inventory component Can now update the state of the single fishes

--- src/components/app.js
+++ src/components/app.js
@@ -12,6 +12,7 @@ class App extends React.Component {
     super();
 
     this.addFish = this.addFish.bind(this);
+    this.updateFish = this.updateFish.bind(this);
     this.loadSamples = this.loadSamples.bind(this);
     this.addToOrder = this.addToOrder.bind(this);
 
@@ -57,6 +58,12 @@ class App extends React.Component {
     this.setState({ fishes });
   }
 
+  updateFish(key, updatedFish) {
+    const fishes = {...this.state.fishes};
+    fishes[key] = updatedFish;
+    this.setState({ fishes });
+  }
+
   loadSamples() {
     this.setState({
       fishes: sampleFishes
@@ -90,7 +97,12 @@ class App extends React.Component {
         order={this.state.order}
         params={this.props.params}
       />
-      <Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
+      <Inventory
+        addFish={this.addFish}
+        loadSamples={this.loadSamples}
+        fishes={this.state.fishes}
+        updateFish={this.updateFish}
+      />
     </div>
     )
   }
--- src/components/inventory.js
+++ src/components/inventory.js
@@ -2,10 +2,45 @@ import React from 'react';
 import AddFishForm from './addfishform'
 
 class Inventory extends React.Component {
+  constructor() {
+    super();
+
+    this.renderInventory = this.renderInventory.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event, key) {
+    const fish = this.props.fishes[key];
+    // take a cojpy of that fish and update with the new data
+    const updatedFish = {
+      ...fish,
+      [event.target.name]: event.target.value
+    }
+    this.props.updateFish(key, updatedFish);
+  }
+
+  renderInventory(key) {
+    const fish = this.props.fishes[key]
+
+    return (
+      <div className="fish-edit" key={key}>
+        <input value={fish.name} type="text" name="name" placeholder="Fish Name" onChange={(event) => this.handleChange(event, key)}/>
+        <input value={fish.price} type="text" name="price" placeholder="Fish Price" onChange={(event) => this.handleChange(event, key)}/>
+        <select value={fish.status} type="text" name="status" placeholder="Fish Status" onChange={(event) => this.handleChange(event, key)}>
+          <option value="available">Fresh!</option>
+          <option value="unavailable">Sold Out</option>
+        </select>
+        <textarea value={fish.desc} name="desc" placeholder="Fish Desc" onChange={(event) => this.handleChange(event, key)}></textarea>
+        <input value={fish.image} name="image" type="text" placeholder="Fish Image" onChange={(event) => this.handleChange(event, key)}/>
+      </div>
+    )
+  }
+
   render() {
     return (
       <div>
         <h2>Inventory</h2>
+        {Object.keys(this.props.fishes).map(this.renderInventory)}
         <AddFishForm addFish={this.props.addFish}/>
         <button onClick={this.props.loadSamples}>Load Sample Fishes</button>
       </div>
